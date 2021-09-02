ff2777ab495565e8d307f4fd290a763aa261cdff
kps81 <ansel7779@gmail.com>
2021-09-02 16:29:56 +0900

    index, guide 수정

--- project/assets/js/ui-common.js
+++ project/assets/js/ui-common.js
@@ -1,19 +1,59 @@
 
 window.addEventListener('DOMContentLoaded', function(){
-  gnbControlGuide(); // gnb
-  toggleObj();
-
+  tabControl("js-tab-head", "js-tab-body"); // tab
+  modalPopControl(); // modal popup
+  gnbControl(); // gnb
+  toggleObj(); // toggle
 });
 
 
+/* tab */
+function tabControl(tabHead, tabBody){
+  const tabBtn = document.querySelectorAll("." + tabHead + " li");
+  const tabContent = document.querySelectorAll("." + tabBody + " > .tabContent");
+
+  tabBtn.forEach(function(obj, i){
+    obj.addEventListener("click",function(e){
+      //e.preventDefault();
+      tabBtn.forEach(function(obj){
+        obj.classList.remove("active");
+      });
+      obj.classList.add("active");
+
+      tabContent.forEach(function(obj, i){
+        obj.classList.remove("on");
+      });
+      tabContent[i].classList.add("on");
+    });
+  });
+}
+
+/* modal popup */
+function modalPopControl() {
+  const modalBtn = document.querySelectorAll(".js-modalPop");
+  modalBtn.forEach(function(obj){
+    obj.addEventListener("click", function(){
+      const modalId = obj.getAttribute("href");
+      const modalWrap = document.querySelector(modalId);
+      const modalClsBtn = document.querySelector(modalId + " .modal-close > button");
+      console.log(modalId);
+      modalWrap.classList.add("open");
+      modalClsBtn.addEventListener("click", function(){
+        modalWrap.classList.remove("open");
+        obj.focus();
+      });
+    });
+  });
+}
+
 /* gnb */
-function gnbControlGuide() {
-  const gnbOpenBtn = document.querySelector(".g-btn-gnb");
-  const gnbWrap = document.querySelector(".g-gnb-wrap");
+function gnbControl() {
+  const gnbOpenBtn = document.querySelector(".btn-gnb");
+  const gnbWrap = document.querySelector(".gnb-wrap");
   const gnbCloseBtn = document.querySelector(".gnb-close");
   const gnbContainer = document.querySelector(".gnb-container");
   const gnbBack = document.createElement("div");
-  gnbBack.classList.add("g-gnb-back");
+  gnbBack.classList.add("gnb-back");
   gnbOpenBtn.addEventListener("click", function(){
     gnbWrap.classList.add("open");
     //console.log(-gnbContainer.offsetWidth);
@@ -23,8 +63,8 @@ function gnbControlGuide() {
         {duration: 200, fill: "forwards"}
       );
       gnbWrap.insertBefore(gnbBack, gnbContainer);
-      document.body.style.overflow = "hidden";
-    }, 50);     
+      document.body.style.overflow = "hidden"; 
+    }, 50);   
   });
   gnbCloseBtn.addEventListener("click", function(){
     gnbContainer.animate(
@@ -39,6 +79,7 @@ function gnbControlGuide() {
   });
 }
 
+/* toggle */
 function toggleObj() {
   const toggleBtn = document.querySelectorAll("[data-toggle-obj]");
   const toggleTarget = document.querySelectorAll("[data-toggle-target]");
@@ -62,7 +103,6 @@ function toggleObj() {
         });
         obj.classList.add("on");
       }
-      
     });
   });
 }
